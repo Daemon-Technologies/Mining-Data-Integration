4c057260db6712a88aa3f055bc0cce3e2775a754
Gavin <tianyaogavin@gmail.com>
2021-04-06 23:44:56 +0800

    adjust bonus stx reward computation

--- utils.js
+++ utils.js
@@ -1,9 +1,10 @@
 export function computeRR(params){
     const {btcPrice, stxPrice, gas, minerData} = params;
-    const stxRewardAmount = 2466
+    const stxRewardAmountBonus = 2466
+    const stxRewardAmountNormal = 1000
     //console.log(minerData)
-    const {actual_win, total_mined, miner_burned} = minerData;
-    let earn = actual_win * stxRewardAmount * stxPrice;
+    const {actual_win, total_mined, miner_burned, actual_win_bonus} = minerData;
+    let earn = actual_win_bonus * stxRewardAmountBonus * stxPrice + (actual_win-actual_win_bonus) * stxRewardAmountNormal * stxPrice;
     let cost = (total_mined * gas + miner_burned) * btcPrice / 100000000;
     //console.log(earn, cost)
     return earn/cost - 1
